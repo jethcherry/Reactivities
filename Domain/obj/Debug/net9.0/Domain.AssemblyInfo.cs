bd4edb6f596eb7dd7395db6f35ff82b3cc77e3d4
jeth cherry <jethrosumbeiywet@gmail.com.com>
2024-12-04 00:49:53 +0300

    added

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2c133356ec28d4fd9c24c6689cc97f6fc77190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80be10e0139747be13c3820a904c232b5dba0341")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
