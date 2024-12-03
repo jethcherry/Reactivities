ad6a7f049beb91e4d63a3d94753052f6458eb77c
jeth cherry <jethrosumbeiywet@gmail.com.com>
2024-12-04 00:43:07 +0300

    addded

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2c133356ec28d4fd9c24c6689cc97f6fc77190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96325881bfa6fabb4e99be7c1667b4665622c122")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
