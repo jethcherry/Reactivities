be42c02ec20a8ee569af8ffca1f8d8be10bcca40
jeth cherry <jethrosumbeiywet@gmail.com.com>
2024-12-05 12:59:03 +0300

    added

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96325881bfa6fabb4e99be7c1667b4665622c122")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4edb6f596eb7dd7395db6f35ff82b3cc77e3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
